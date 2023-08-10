596a1cab2738e2a477db06a93ac2fee9b4240f16
GabrielSKlauck <112292289+GabrielSKlauck@users.noreply.github.com>
2023-08-10 20:13:45 -0300

    Botao novo funcionando, mas sem tecla de atalho

--- Interface/src/Principal.java
+++ Interface/src/Principal.java
@@ -17,11 +17,13 @@ import javax.swing.UIManager;
 import java.awt.Color;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.Font;
+import javax.swing.JSeparator;
 
 public class Principal extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
+	private JTextField tfArquivo;
 
 	/**
 	 * Launch the application.
@@ -57,6 +59,7 @@ public class Principal extends JFrame {
 	 * Create the frame.
 	 */
 	public Principal() {
+		setTitle("COMPILADOR");
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 937, 640);
@@ -65,11 +68,47 @@ public class Principal extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+				
+		TextArea tfCodigo = new TextArea();
+		tfCodigo.setText("\r\n");
+		tfCodigo.setFont(new Font("Courier New", Font.PLAIN, 12));
+		tfCodigo.setBounds(20, 54, 901, 349);
+		contentPane.add(tfCodigo);
+			
+		TextArea tfMensagens = new TextArea();
+		tfMensagens.setEditable(false);
+		tfMensagens.setBounds(0, 401, 921, 167);
+		contentPane.add(tfMensagens);
 		
+		tfArquivo = new JTextField();
+		tfArquivo.setText("Sem arquivo aberto");
+		tfArquivo.setToolTipText("");
+		tfArquivo.setEditable(false);
+		tfArquivo.setBackground(new Color(240, 240, 240));
+		tfArquivo.setBounds(0, 569, 921, 25);
+		contentPane.add(tfArquivo);
+		tfArquivo.setColumns(10);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setLineWrap(true);
+		textArea.setEditable(false);
+		textArea.setEnabled(false);
+		textArea.setWrapStyleWord(true);
+		textArea.setFont(new Font("Courier New", Font.PLAIN, 12));
+		textArea.setBackground(new Color(240, 240, 240));
+		textArea.setText("1\r\n2\r\n3\r\n4\r\n5\r\n6\r\n7\r\n8\r\n9\r\n10\r\n11\r\n12\r\n13\r\n14\r\n15\r\n16");
+		textArea.setBounds(0, 54, 19, 330);
+		contentPane.add(textArea);
+		
+		
+		//Funçoes dos botoes
 		JButton btnNovo = new JButton("Novo");
 		btnNovo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(null, "Teste de botao");
+				tfCodigo.setText("");
+				tfMensagens.setText("");
+				tfArquivo.setText("");
+				
 			}
 		});
 		
@@ -107,21 +146,5 @@ public class Principal extends JFrame {
 		JButton btnEquipe = new JButton("Equipe");
 		btnEquipe.setBounds(630, 0, 89, 45);
 		contentPane.add(btnEquipe);
-		
-		TextArea textArea = new TextArea();
-		textArea.setBounds(0, 54, 921, 349);
-		contentPane.add(textArea);
-		
-		TextArea textArea_1 = new TextArea();
-		textArea_1.setEditable(false);
-		textArea_1.setBounds(0, 401, 921, 167);
-		contentPane.add(textArea_1);
-		
-		textField = new JTextField();
-		textField.setEditable(false);
-		textField.setBackground(new Color(240, 240, 240));
-		textField.setBounds(0, 569, 921, 25);
-		contentPane.add(textField);
-		textField.setColumns(10);
 	}
 }
