2d1f69a26c2ffe4184907c5f99b77793b4d54e5e
GabrielSKlauck <112292289+GabrielSKlauck@users.noreply.github.com>
2023-08-22 16:08:23 -0300

    Varios incrementos
    Atalhos dos botoes funcionais e a tela nao diminui mais do que 1115 pixels de largura e 700 pixels de altura

--- Interface/src/Principal.java
+++ Interface/src/Principal.java
@@ -9,25 +9,36 @@ import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFileChooser;
 
 import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextArea;
 import java.awt.TextArea;
 import javax.swing.JTextField;
+import javax.swing.KeyStroke;
 import javax.swing.UIManager;
 import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.awt.Font;
 import javax.swing.JSeparator;
 import javax.swing.SwingConstants;
 import javax.swing.JSlider;
+import javax.swing.AbstractAction;
 import javax.swing.ImageIcon;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -35,6 +46,9 @@ import javax.swing.JSplitPane;
 import javax.swing.JScrollBar;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.border.LineBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.Dimension;
+import java.awt.Rectangle;
 
 public class Principal extends JFrame {
 
@@ -75,7 +89,8 @@ public class Principal extends JFrame {
 	 * Create the frame.
 	 */
 	public Principal() {
-		
+		setMinimumSize(new Dimension(1115, 700));
+			
 		setTitle("COMPILADOR");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1114, 685);
@@ -129,11 +144,20 @@ public class Principal extends JFrame {
 		contentPane.add(tfMensagens);
 		//###### Funçoes dos botoes ########
 		
-		
+		//Botao novo
 		JButton btnNovo = new JButton("Novo\r\n[crtl + n]");
 		btnNovo.setIcon(new ImageIcon(Principal.class.getResource("/icones/novo-documento.png")));
 		btnNovo.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		
+		btnNovo.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("control N"), "Novo");
+		btnNovo.getActionMap().put("Novo", new AbstractAction(){
+			public void actionPerformed(ActionEvent e){
+				tfCodigo.setText("");
+				tfMensagens.setText("");
+				tfArquivo.setText("");
+		     }
+
+		  });
 		btnNovo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				tfCodigo.setText("");
@@ -146,8 +170,51 @@ public class Principal extends JFrame {
 		btnNovo.setBounds(0, 0, 120, 45);
 		contentPane.add(btnNovo);
 		
+		//---------
+		
+		//botao abrir
+		
 		JButton btnAbrir = new JButton("Abrir[ctrl + o]");
 		btnAbrir.setIcon(new ImageIcon(Principal.class.getResource("/icones/pasta-aberta.png")));
+		
+		btnNovo.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("control O"), "Abrir");
+		btnNovo.getActionMap().put("Abrir", new AbstractAction(){
+			public void actionPerformed(ActionEvent e){
+				EscolheArquivo.setEnabled(true);
+				EscolheArquivo.setVisible(true);
+				try {
+				String arquivo = getArquivo(EscolheArquivo);
+				File file = new File(arquivo);
+				FileInputStream fis = null;
+		        String texto = "";
+		        
+		        try {
+		            fis = new FileInputStream(file);
+		            int content;
+		            while ((content = fis.read()) != -1) {
+		                texto += (char) content;
+		            }
+		        } catch (IOException el) {
+		            el.printStackTrace();
+		        } finally {
+		            try {
+		                if (fis != null) {
+		                    fis.close();
+		                }
+		            } catch (IOException ex) {
+		                ex.printStackTrace();
+		            }
+		        }
+		        tfCodigo.setText(texto);	
+		        tfMensagens.setText("");
+		        tfArquivo.setText(arquivo);
+		        }catch(NullPointerException et) {
+		        	
+		        }			    	    			    				
+		     }
+
+		  });
+		
 		btnAbrir.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
@@ -177,37 +244,72 @@ public class Principal extends JFrame {
 		                ex.printStackTrace();
 		            }
 		        }
-		        tfCodigo.setText(texto);
+		        tfCodigo.setText(texto);	
+		        tfMensagens.setText("");
 		        tfArquivo.setText(arquivo);
 		        }catch(NullPointerException et) {
 		        	
-		        }
-		        
-		        
-				
-				
-				
-				
-			    	    			    	
+		        }			    	    			    	
 			}
 		});
 		btnAbrir.setBounds(125, 0, 120, 45);
 		contentPane.add(btnAbrir);
 		
-		
+		//--------------
+		//botao salvar
 		
 		JButton btnSalvar = new JButton("Salvar[ctrl + s]");
+		
+		btnNovo.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("control S"), "Salvar");
+		btnNovo.getActionMap().put("Salvar", new AbstractAction(){
+			public void actionPerformed(ActionEvent e){
+				if(tfArquivo.getText().isEmpty()) {
+					EscolheArquivo.setEnabled(true);
+					EscolheArquivo.setVisible(true);
+					File file = new File(getArquivo(EscolheArquivo));
+					System.out.print(file);
+				}else {
+					try {
+						BufferedWriter buffWrite = new BufferedWriter(new FileWriter(tfArquivo.getText(), true));					
+						buffWrite.write(tfCodigo.getText());
+					} catch (IOException e1) {
+						
+						e1.printStackTrace();
+					}
+				}
+			}
+		});
+		
+		btnSalvar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				if(tfArquivo.getText().isEmpty()) {
+					EscolheArquivo.setEnabled(true);
+					EscolheArquivo.setVisible(true);
+					File file = new File(getArquivo(EscolheArquivo));
+					System.out.print(file);
+				}else {
+					try {
+						BufferedWriter buffWrite = new BufferedWriter(new FileWriter(tfArquivo.getText(), true));					
+						buffWrite.write(tfCodigo.getText());
+					} catch (IOException e1) {
+						
+						e1.printStackTrace();
+					}
+				}
+				
+					        
+			}
+		});
 		btnSalvar.setIcon(new ImageIcon(Principal.class.getResource("/icones/opcao-de-salvar-arquivo.png")));
 		btnSalvar.setBounds(252, 0, 125, 45);
 		contentPane.add(btnSalvar);
 		
+		//--------------------------
+		
 		//##########  Ja vem por padrao  #################
 		
-		JButton btnCopiar = new JButton("Copiar[ctrl + c]");
-		btnCopiar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
+		JButton btnCopiar = new JButton("Copiar[ctrl + c]");		
 		btnCopiar.setIcon(new ImageIcon(Principal.class.getResource("/icones/copia-de.png")));
 		btnCopiar.setBounds(382, 0, 125, 45);
 		contentPane.add(btnCopiar);
@@ -224,6 +326,7 @@ public class Principal extends JFrame {
 		
 		//####################
 		
+		//Compilacao
 		JButton btnCompilar = new JButton("Compilar[F7]");
 		btnCompilar.setIcon(new ImageIcon(Principal.class.getResource("/icones/engrenagem.png")));
 		btnCompilar.addActionListener(new ActionListener() {
@@ -231,22 +334,47 @@ public class Principal extends JFrame {
 				tfMensagens.setText("Compilação de programas ainda não implementada");
 			}
 		});
+		
+		btnCompilar.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("F7"), "Compilar");
+		btnCompilar.getActionMap().put("Compilar", new AbstractAction(){
+			public void actionPerformed(ActionEvent e){
+		        tfMensagens.setText("Compilaçao de programas ainda nao implementada");
+		     }
+
+		  });
+		
+		
+		
 		btnCompilar.setBounds(790, 0, 120, 45);
 		contentPane.add(btnCompilar);
 		
+		//---------------
+		//Equipe
 		JButton btnEquipe = new JButton("Equipe[F1]");
 		btnEquipe.setIcon(new ImageIcon(Principal.class.getResource("/icones/equipe.png")));
+		
+		btnCompilar.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("F1"), "Equipe");
+		btnCompilar.getActionMap().put("Equipe", new AbstractAction(){
+			public void actionPerformed(ActionEvent e){
+		        tfMensagens.setText("Gabriel de Souza Klauck, Nycolly Miranda");
+		     }
+
+		  });
+		
 		btnEquipe.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				tfMensagens.setText("Gabriel de Souza Klauck, Nycolly Miranda");
 			}
 		});
+		
 		btnEquipe.setBounds(915, 0, 105, 45);
 		contentPane.add(btnEquipe);
 		
 	}
 	
+	//Metodo seleciona um arquivo para abrir e retorna o caminho desse arquivo
 	public static String getArquivo(JFileChooser escolha) {
+		escolha.setFileFilter(new FileNameExtensionFilter("Arquivos *.txt", "txt"));
 		int returnVal = escolha.showOpenDialog(escolha);
 	    if (returnVal == JFileChooser.APPROVE_OPTION) {
 	        String file = "" + escolha.getSelectedFile();
@@ -254,4 +382,16 @@ public class Principal extends JFrame {
 	    }
 	    return null;
 	}
+	
+	/*public static String setArquivo(JFileChooser escolha) {
+		escolha.setFileFilter(new FileNameExtensionFilter("Arquivos *.txt", "txt"));
+		escolha.setCurrentDirectory(new File(System.getProperty("user.home")));
+		int returnVal = escolha.showOpenDialog(escolha);
+	    if (returnVal == JFileChooser.APPROVE_OPTION) {
+	        String file = "" + escolha.getSelectedFile();
+	        return file;
+	    }
+	    return null;
+	}*/
+	
 }
